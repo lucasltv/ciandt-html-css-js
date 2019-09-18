7b70fa6bc3f7d4a2a6b8fadea963a5d4588933ab
Lucas Thomaz Vieira <lucasv@lnxcit022084.cit>
2019-09-18 15:15:24 -0300

    hands-on-10 finished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
 
-window.addEventListener('load', mostrarNotificacaoBoasVindas);
+window.addEventListener('load', boot);
+
+function boot() {
+    var removeBtn = document.querySelector("#removeBtn");
+    removeBtn.addEventListener('click', removeLoremIpsumSection);
+    mostrarNotificacaoBoasVindas();
+
+    var allLinksWithMouseHoverEfectArray = document.getElementsByClassName("mouseHoverEfect");
+    for (let index = 0; index < allLinksWithMouseHoverEfectArray.length; index++) {
+        var span = allLinksWithMouseHoverEfectArray[index];
+        addMouseOverEfect(span);
+    }
+}
 
 function mostrarNotificacaoBoasVindas() {
     alert('Olá! Seja bem vindo à página inicial!');
+}
+
+function removeLoremIpsumSection(e) {
+    var loremIpsumSection = document.getElementById('loremIpsumSection');
+    loremIpsumSection.remove();
+    this.remove();
+    document.body.style.backgroundColor = "#FFFFFF";
+}
+
+function addMouseOverEfect(span) {
+    span.addEventListener('mouseenter', function (e) {
+        this.style.fontWeight = "bold";
+    });
+    span.addEventListener('mouseleave', function (e) {
+        this.style.fontWeight = "normal";
+    })
 }
