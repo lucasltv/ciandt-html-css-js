23e8961c860d3b9597469a74397ece90b13fc066
lucasltv <lucasltv@gmail.com>
2019-09-23 21:40:27 -0300

    Desafio (hands-on-13)

--- js/table.js
+++ js/table.js
@@ -1,11 +1,17 @@
 var urlApi = "https://ciandt-html-css-js.herokuapp.com/api/users";
 
+var usuarios = [];
+var ordenacao = true; //true=asc e false=desc
+var selecionado = "nome";
 $(document).ready(function () {
     buscaUsuarios();
+    $("th").on("click", alteraOrdenacao)
 });
 
-function tratarSucesso(usuarios) {
+function renderizaTabela(usuariosParaRenderizar) {
     var tbody = $("table tbody");
+    tbody.html("");
+    usuarios = usuariosParaRenderizar;
     usuarios.forEach(usuario => {
         tbody.append("<tr>" +
             "<td>" + usuario.id + "</td>" +
@@ -28,9 +34,51 @@ function buscaUsuarios() {
         url: urlApi,
         type: "get",
     })
-        .done(tratarSucesso)
+        .done(renderizaTabela)
         .fail(tratarErro)
         .always(function () {
             tituloTabela.text("TABELA DINÂMICA");
         })
+}
+
+function alteraOrdenacao(event) {
+    var id = this.id;
+    switch (id) {
+        case "thId":
+            ordenaTabela('id');
+            break;
+        case "thNome":
+            ordenaTabela('nome');
+            break;
+        case "thTelefone":
+            ordenaTabela('telefone');
+            break;
+        case "thEmail":
+            ordenaTabela('email');
+            break;
+        default:
+            return;
+    }
+    renderizaSeta(id);
+}
+
+function renderizaSeta(id) {
+    $("#thId a").text("ID");
+    $("#thNome a").text("Nome");
+    $("#thTelefone a").text("Telefone");
+    $("#thEmail a").text("Email");
+    var selecionado = $("#" + id + " a");
+    var seta = ordenacao ? " &darr;" : " &uarr;";
+    selecionado.html(selecionado.text() + seta);
+}
+
+function ordenaTabela(coluna) {
+    if (coluna === selecionado) {
+        ordenacao = !ordenacao;
+        renderizaTabela(usuarios.reverse());
+    } else {
+        selecionado = coluna;
+        ordenacao = true;
+        renderizaTabela(_.sortBy(usuarios, coluna, "asc"));
+    }
 }
